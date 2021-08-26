ec1abf52b75987f91ca6d41d8e165efd31fc3200
Douglas Bett <bettdalpha@gmail.com>
2021-08-26 21:49:50 +0300

    added Dockerfile with directions to deploy to cloud run

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,26 @@
+import 'dart:io';
+
 import 'package:grpc/grpc.dart';
 import 'package:hospitals/src/server.dart';
 import 'package:hospitals/src/utils.dart';
 
 void main(List<String> arguments) async {
+  var port = Platform.environment['PORT'];
+  if (port == null) {
+    throw Exception('Port variable is not defined');
+  }
+
+  final intPort = int.parse(port);
+
   final hospitalsData = await Utils.readHospitalsFromCSV();
 
   final server = Server([
     HospitalServer(
-      hospitalsData,
-    )
+      hospitalData: hospitalsData,
+    ),
   ]);
 
-  await server.serve(address: 'localhost', port: 12345);
+  await server.serve(port: intPort);
+
+  print('Server running at port ${server.port}');
 }
